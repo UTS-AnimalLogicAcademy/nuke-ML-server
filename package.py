b771dc3fbcb142e8efd96f05412adc57e99d55e1
daniel flood <daniel.flood-1@uts.edu.au>
2020-08-18 23:28:03 +1000

    added rez package.py, modified hint for nuke installation

---
+++ package.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+name = 'nuke_ML_server'
+
+version = '0.1.3'
+
+build_requires = [
+    'cmake-3.10+'
+]
+
+variants = [
+            ['platform-linux', 'arch-x86_64', 'nuke-12.2']
+           ]
+
+def commands():
+     env.NUKE_PATH.append('{root}/lib')
+     env.LDD_LIBRARY_PATH.append(env.NUKE_LOCATION)
